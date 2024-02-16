2a81ed9fc35c583797254416297f8fdd06cab54e
sharat <sharat@farefirst.com>
2024-02-16 17:26:47 +0530

    state- Declarative programmig

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import Form from "./Form";
 
-var userIsRegistered = false;
 
 function App() {
-  return (
-    <div className="container">
-      <Form 
-      isRegistered = {userIsRegistered}
-      />
-    </div>
-  );
+    var isDone = false;
+
+    const strikethrough = {textDecoration: "line-through"};
+    
+    return <p style={isDone === true ? strikethrough : null}>Buy Milk</p>
+
 }
+
+
+// var userIsRegistered = false;
+// function App() {
+//   return (
+//     <div className="container">
+//       <Form 
+//       isRegistered = {userIsRegistered}
+//       />
+//     </div>
+//   );
+// }
 
 export default App;
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -15,4 +15,4 @@ function Form(props) {
         );
       }
 
-export default Form;
+export default Form;    
